994bbfca326214b4fd243ecdcbc66c5ef99dbc1e
Haruko-Tao <polanickovlad70@gmail.com>
2026-07-13 00:34:33 +0300

    Закончил с созданием rich - модель для Department.cs

---
+++ backend/DirectoryService/src/DirectoryService.Domain/Departments/Result.cs
@@ -0,0 +1,19 @@
+﻿namespace DirectoryService.Domain.Departments;
+#pragma warning disable CA1000
+public class Result<T>
+{
+    public bool IsSuccess { get; }
+    public T? Value { get; }
+    public string? Error { get; }
+
+    private Result(bool isSuccess, T? value, string? error)
+    {
+        IsSuccess = isSuccess;
+        Value = value;
+        Error = error;
+    }
+
+    public static Result<T> Success(T value) => new(true, value, null);
+    public static Result<T> Failure(string error) => new(false, default, error);
+}
+#pragma warning restore CA1000
---
+++ backend/DirectoryService/src/DirectoryService.Domain/Departments/Slug.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace DirectoryService.Domain.Departments;
+
+public class Slug
+{
+    public string Value { get; }
+
+    private Slug(string value)
+    {
+        Value = value;
+    }
+
+    public static Result<Slug> Create(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            return Result<Slug>.Failure("Slug не может быть пустым");
+
+        if (!Regex.IsMatch(value, "^[a-z0-9-]+$", RegexOptions.None, TimeSpan.FromSeconds(1)))
+            return Result<Slug>.Failure("Slug может содержать только строчные латинские буквы, цифры и дефис");
+
+        return Result<Slug>.Success(new Slug(value));
+    }
+}
