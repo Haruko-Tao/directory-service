625d30dfbfccce66c0d2f23d96e158942a5db773
Haruko-Tao <polanickovlad70@gmail.com>
2026-07-15 12:41:45 +0300

    Написан Domain

---
+++ backend/DirectoryService/src/DirectoryService.Domain/Locations/Location.cs
@@ -0,0 +1,32 @@
+﻿using DirectoryService.Domain.Departments;
+
+namespace DirectoryService.Domain.Locations;
+
+public class Location
+{
+    public Guid Id { get; private set; }
+    
+    public Name Name { get; private set; }
+    
+    public Address Address { get; private set; }
+    
+    public DateTime CreatedAt { get; private set; }
+    
+    public DateTime UpdatedAt { get; private set; }
+
+    private Location(Guid id, Name name, Address address, DateTime creadetAt, DateTime updatedAt)
+    {
+        Id = id;
+        Name = name;
+        Address = address;
+        CreatedAt = creadetAt;
+        UpdatedAt = updatedAt;
+    }
+
+    public static Result<Location> Create(Name name, Address address)
+    {
+        var location = new Location(Guid.NewGuid(), name, address, DateTime.UtcNow, DateTime.UtcNow);
+
+        return Result<Location>.Success(location);
+    }
+}
