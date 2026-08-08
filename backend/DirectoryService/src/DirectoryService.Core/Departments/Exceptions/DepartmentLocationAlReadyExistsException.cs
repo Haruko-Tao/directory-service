911537d5c5ae8ff60998e3d8b7caaa876a4231cd
Haruko-Tao <polanickovlad70@gmail.com>
2026-08-08 16:22:15 +0300

    feat(exceptions): add typed domain errors and exception middleware

---
+++ backend/DirectoryService/src/DirectoryService.Core/Departments/Exceptions/DepartmentLocationAlReadyExistsException.cs
@@ -0,0 +1,13 @@
+﻿using DirectoryService.SharedKernel;
+
+namespace DirectoryService.Core.Departments.Exceptions;
+
+public class DepartmentLocationAlReadyExistsException : DomainException
+{
+    public DepartmentLocationAlReadyExistsException(Guid departmentId, Guid locationId) : base(Error.Conflict("exist.department.location",
+        $"Связь с {departmentId} и {locationId} уже существует"))
+
+    {
+
+    }
+}
---
+++ backend/DirectoryService/src/DirectoryService.Core/Departments/Exceptions/DepartmentNotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using DirectoryService.SharedKernel;
+
+namespace DirectoryService.Core.Departments.Exceptions;
+
+public class DepartmentNotFoundException : DomainException
+{
+    public DepartmentNotFoundException(Guid id) : base(
+        Error.NotFound("department.not.found", $"Отдел с {id} не найден"))
+    {
+
+    }
+}
